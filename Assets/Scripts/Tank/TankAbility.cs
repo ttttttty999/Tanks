935d2ce1869b79b4d134cdad13c29a33dd27f6b5
Klein4869 <thd1998@live.cn>
2018-12-14 01:47:36 +0800

    []

--- Assets/Scripts/Tank/TankAbility.cs
+++ Assets/Scripts/Tank/TankAbility.cs
@@ -10,41 +10,55 @@ public class TankAbility : MonoBehaviour
     public TankMovement tankMovement;
 
 
-    private float m_CdTime;
+    private float m_CdTime1;
     private float m_AbilityTime;
     private string m_AbilityInputName;
+    private string m_SquarePlaceInputName;
     private bool m_inAbility;
     private bool m_AbilityInputValue;
+    private bool m_SquarePlaceInputValue;
 
     private void OnEnable()
     {
         m_AbilityInputName = "Ability" + m_PlayerNumber;
+        m_SquarePlaceInputName = "SquarePlace" + m_PlayerNumber;
         m_AbilityInputValue = false;
+        m_SquarePlaceInputValue = false;
         m_inAbility = false;
-        m_CdTime = 0f;
+        m_CdTime1 = 0f;
         m_AbilityTime = 5f;
     }
 
     private void Update()
     {
         m_AbilityInputValue = Input.GetButton(m_AbilityInputName);
+        m_SquarePlaceInputValue = Input.GetButton(m_SquarePlaceInputName);
     }
 
     private void FixedUpdate()
     {
         Ability1();
+        SquarePlace();
+    }
+
+    private void SquarePlace()
+    {
+        if (m_SquarePlaceInputValue)
+        {
+            
+        }
     }
 
     private void Ability1()
     {
         //技能1
-        print(m_CdTime);
-        m_CdTime += Time.deltaTime;
+        print(m_CdTime1);
+        m_CdTime1 += Time.deltaTime;
         if (m_PlayerNumber == 1)
         {
-            if (m_AbilityInputValue && m_CdTime >= 10f && !m_inAbility)
+            if (m_AbilityInputValue && m_CdTime1 >= 10f && !m_inAbility)
             {
-                tankMovement.m_Speed = 1.5f * tankMovement.m_Speed;
+                tankMovement.m_Speed = 2f * tankMovement.m_Speed;
                 m_inAbility = true;
             }
 
@@ -58,7 +72,7 @@ public class TankAbility : MonoBehaviour
                 m_inAbility = false;
                 tankMovement.m_Speed = tankMovement.m_OriginalSpeed;
                 m_AbilityTime = 5f;
-                m_CdTime = 0f;
+                m_CdTime1 = 0f;
             }
         }
 
